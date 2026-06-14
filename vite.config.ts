6ee859e307f14ae722662b77fcc751b9094c2a84
Verzach3 <gabrielcorreacardenas@gmail.com>
2026-06-13 23:09:42 -0500

    chore: setup react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
+import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 
 /// <reference types="@batijs/core/types" />
 
@@ -8,7 +9,15 @@ import vike from "vike/plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [vike(), react(), tailwindcss(), evlog({ service: "zentro" })],
+  plugins: [
+    vike(),
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+    tailwindcss(),
+    evlog({ service: "zentro" }),
+  ],
 
   server: {
     allowedHosts: ["host.docker.internal"],
